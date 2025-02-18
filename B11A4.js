45387d9a9613bba29bc4213674ecaab6f669ac5f
niloyahmadramjan <niloyahmadramjan@gmail.com>
2025-02-18 14:13:33 +0800

    problem solve 2,3

--- B11A4.js
+++ B11A4.js
@@ -9,5 +9,34 @@ function calculateVAT(price) {
   }
 }
 
+/*Problem 02 */
+function validContact(contact) {
+  if (typeof contact !== "string") {
+    return "Invalid";
+  } else if (
+    contact.length === 11 &&
+    contact.includes() !== " " &&
+    contact.slice(0, 2) == "01"
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-
+/*Problem 03*/
+function willSuccess(marks) {
+  if (!Array.isArray(marks)) {
+    return "Invalid";
+  }
+  let upto50 = [];
+  let downto50 = [];
+  for (let mark of marks) {
+    if (mark >= 50) {
+      upto50.push(mark);
+    } else if (mark <= 49) {
+      downto50.push(mark);
+    }
+  }
+  return upto50.length > downto50.length;
+}
